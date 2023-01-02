da967c17c52e2003288f36f09b0bd55e3a062e49
Lucash <lucas565lkj@gmail.com>
2023-01-02 13:26:33 -0300

    feat: create mocks

---
+++ src/mocks/index.tsx
@@ -0,0 +1,5 @@
+export const mainMocks = {
+  title: 'Todas as suas compras em um aplicativo',
+  description:
+    'Venda seus dispositivos da maneira mais inteligente e rápida para obter dinheiro imediato e uma consciência mais limpa.',
+};
