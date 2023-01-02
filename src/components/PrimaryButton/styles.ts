e376ab8623cf6f88bd12fb8666017f298f67eeb8
Lucash <lucas565lkj@gmail.com>
2023-01-02 13:25:06 -0300

    style: fix styles

--- src/components/PrimaryButton/styles.ts
+++ src/components/PrimaryButton/styles.ts
@@ -1,11 +1,9 @@
 import styled from 'styled-components/native';
 
 export const StyledTouchableOpacity = styled.TouchableOpacity`
+  flex-grow: 1;
   justify-content: center;
   align-items: center;
-  padding: 14px 0;
-  gap: 10px;
-  position: relative;
   height: 45px;
   background-color: ${({theme}) => theme.colors.darkGreen};
   border-radius: 10px;
@@ -15,5 +13,5 @@ export const Styledtext = styled.Text`
   font-family: 'Inter-Bold';
   font-size: 14px;
   line-height: 17px;
-  color: #fff;
+  color: ${({theme}) => theme.colors.white};
 `;
