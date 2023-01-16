4062cb34e5386064aff95d62438f35ebc88f9dcd
Lucash <lucas565lkj@gmail.com>
2023-01-16 14:01:51 -0300

    build: add dependencies and allow to use svg images

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,10 @@ declare module '*.png' {
   const value: import('react-native').ImageSourcePropType;
   export default value;
 }
+
+declare module '*.svg' {
+  import React from 'react';
+  import {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
