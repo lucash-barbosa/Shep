e6a6077636c962320410b90751e07fe50ada4fff
Lucash <lucas565lkj@gmail.com>
2023-01-16 13:59:41 -0300

    feat: create interface for navigate

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 import splashScreen from '../../assets/gifs/SplashScreen.json';
 import {StyledView} from './styled';
+import {StackProps} from '../../interfaces/Navigation';
 
-const SplashScreen = ({navigation}: any) => {
+const SplashScreen = ({navigation}: StackProps) => {
   const animationOnFinish = () => {
     navigation.reset({
       index: 0,
       routes: [{name: 'Main'}],
     });
   };
-
   return (
     <StyledView>
       <LottieView
